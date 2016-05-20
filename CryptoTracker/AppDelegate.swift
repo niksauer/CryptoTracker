36e3b9d3db02173fb1e4fa8dd2326fdb06d989ec
Nik Sauer <nik.sauer@me.com>
2016-05-20 14:42:05 +0200

    Initial Commit

---
+++ CryptoTracker/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  CryptoTracker
+//
+//  Created by Niklas Sauer on 20.05.16.
+//  Copyright © 2016 Niklas Sauer. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
