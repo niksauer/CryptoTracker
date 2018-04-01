f7d5ecf58469d587668b73ea56e14908e205cdd7
Niklas Sauer <nik.sauer@me.com>
2018-04-01 20:07:04 +0200

    update to swift 3 syntax

--- CryptoTracker/AppDelegate.swift
+++ CryptoTracker/AppDelegate.swift
@@ -8,31 +8,35 @@
 
 import Cocoa
 
+enum KrakenAPIError: Error {
+    case invalidJSON
+}
+
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
 
-    let defaults = NSUserDefaults.standardUserDefaults()
+    let defaults = UserDefaults.standard
     
-    let statusBarItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1)
+    let statusBarItem = NSStatusBar.system().statusItem(withLength: -1)
     let option1: NSMenuItem = NSMenuItem(title: "ETH/EUR", action: #selector(AppDelegate.callSetSelection), keyEquivalent: "")
     let option2: NSMenuItem = NSMenuItem(title: "ETH/BTC", action: #selector(AppDelegate.callSetSelection), keyEquivalent: "")
     
-    func applicationDidFinishLaunching(aNotification: NSNotification) {
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
         let menu: NSMenu = NSMenu()
         
         menu.addItem(NSMenuItem(title: "Update", action: #selector(AppDelegate.updatePrice), keyEquivalent: ""))
-        menu.addItem(NSMenuItem.separatorItem())
+        menu.addItem(NSMenuItem.separator())
         menu.addItem(NSMenuItem(title: "Open Chart", action: #selector(AppDelegate.openURL), keyEquivalent: "o"))
         menu.addItem(NSMenuItem(title: "Open Exchange", action: #selector(AppDelegate.openURL), keyEquivalent: ""))
-        menu.addItem(NSMenuItem.separatorItem())
+        menu.addItem(NSMenuItem.separator())
         
         let submenu: NSMenu = NSMenu(title: "Price")
         let submenuItem: NSMenuItem = NSMenuItem(title: "Set Price", action: nil, keyEquivalent: ",")
         
         menu.addItem(submenuItem)
-        menu.setSubmenu(submenu, forItem: submenuItem)
+        menu.setSubmenu(submenu, for: submenuItem)
         
         submenu.addItem(option1)
         submenu.addItem(option2)
@@ -42,47 +46,60 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         statusBarItem.menu = menu
     
         checkForDefaults()
-        statusBarItem.title = getPrice()
+        updatePrice()
         
-        NSTimer.scheduledTimerWithTimeInterval(5, target: self, selector: #selector(AppDelegate.updatePrice), userInfo: nil, repeats: true)
+        Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(AppDelegate.updatePrice), userInfo: nil, repeats: true)
     }
     
-    func openURL(sender: AnyObject) {
+    func openURL(_ sender: AnyObject) {
         let exchange = "https://www.kraken.com/en-us/login"
         let chart = "https://cryptowatch.de/kraken/etheur"
         
         if sender.title == "Open Chart" {
-            NSWorkspace.sharedWorkspace().openURL(NSURL(string: chart)!)
+            NSWorkspace.shared().open(URL(string: chart)!)
         } else if sender.title == "Open Exchange" {
-            NSWorkspace.sharedWorkspace().openURL(NSURL(string: exchange)!)
+            NSWorkspace.shared().open(URL(string: exchange)!)
         }
     }
     
-    func getJSON(urlToRequest: String) -> NSData {
-        return NSData(contentsOfURL: NSURL(string: urlToRequest)!)!
+    func getJSON(_ urlToRequest: String) -> Data {
+        return (try! Data(contentsOf: URL(string: urlToRequest)!))
     }
     
-    func getPrice() -> String {
-        let inputData = getJSON("https://api.kraken.com/0/public/Ticker?pair=" + defaults.stringForKey("setPrice")!)
+    func getPrice() throws -> String {
+        let inputData = getJSON("https://api.kraken.com/0/public/Ticker?pair=" + defaults.string(forKey: "setPrice")!)
         
-        do {
-            let ticker = try NSJSONSerialization.JSONObjectWithData(inputData, options: [])
-            let bidArray = ticker["result"]??[defaults.stringForKey("setPair")!]??["c"] as! NSArray
-            let price = bidArray[0] as! String
-            let index = price.startIndex.advancedBy(5)
-            return "Ξ" + price.substringToIndex(index)
-        } catch let error as NSError {
-            print("An error occurred: \(error)")
-            return("ERR")
+        let jsonObject = try JSONSerialization.jsonObject(with: inputData, options: [])
+        
+        guard let jsonDictionary = jsonObject as? [String: Any], let tickerDictionary = jsonDictionary["result"] as? [String: Any] else {
+            throw KrakenAPIError.invalidJSON
         }
+        
+        guard let selectedPairDictionary = tickerDictionary[defaults.string(forKey: "setPair")!] as? [String: Any] else {
+            return ""
+        }
+        
+//        let currentPrice =
+        
+        
+        //            let bidArray = ticker["result"][defaults.string(forKey: "setPair")!]??["c"] as! NSArray
+        //            let price = bidArray[0] as! String
+        //            let index = price.characters.index(price.startIndex, offsetBy: 5)
+        //            return "Ξ" + price.substring(to: index)
+        
+        return "test"
     }
     
     func updatePrice() {
-        let newPrice = getPrice()
-        statusBarItem.title = newPrice
+        do {
+            let newPrice = try getPrice()
+            statusBarItem.title = newPrice
+        } catch {
+            // catch API error
+        }
     }
     
-    func callSetSelection(sender: AnyObject) {
+    func callSetSelection(_ sender: AnyObject) {
         if sender.title == "ETH/EUR" {
             setSelection("ETHEUR")
         } else if sender.title == "ETH/BTC" {
@@ -90,33 +107,33 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         }
     }
     
-    func setSelection(selection: String) {
+    func setSelection(_ selection: String) {
         if selection == "ETHEUR" {
-            defaults.setObject("ETHEUR", forKey: "setPrice")
-            defaults.setObject("XETHZEUR", forKey: "setPair")
+            defaults.set("ETHEUR", forKey: "setPrice")
+            defaults.set("XETHZEUR", forKey: "setPair")
             option1.title = "ETH/EUR ✓"
             option2.title = "ETH/BTC"
         } else if selection == "ETHXBT" {
-            defaults.setObject("ETHXBT", forKey: "setPrice")
-            defaults.setObject("XETHXXBT", forKey: "setPair")
+            defaults.set("ETHXBT", forKey: "setPrice")
+            defaults.set("XETHXXBT", forKey: "setPair")
             option1.title = "ETH/EUR"
             option2.title = "ETH/BTC ✓"
         }
     }
     
     func checkForDefaults() {
-        if (defaults.stringForKey("setPrice") == nil) {
-            defaults.setObject("ETHEUR", forKey: "setPrice")
-            defaults.setObject("XETHZEUR", forKey: "setPair")
+        if (defaults.string(forKey: "setPrice") == nil) {
+            defaults.set("ETHEUR", forKey: "setPrice")
+            defaults.set("XETHZEUR", forKey: "setPair")
             setSelection("ETHEUR")
-        } else if (defaults.stringForKey("setPrice") == "ETHEUR") {
+        } else if (defaults.string(forKey: "setPrice") == "ETHEUR") {
             setSelection("ETHEUR")
-        } else if (defaults.stringForKey("setPrice") == "ETHXBT") {
+        } else if (defaults.string(forKey: "setPrice") == "ETHXBT") {
             setSelection("ETHXBT")
         }
     }
 
-    func applicationWillTerminate(aNotification: NSNotification) {
+    func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
 }
